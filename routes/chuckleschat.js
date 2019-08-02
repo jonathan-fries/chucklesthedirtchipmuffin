73e990292e7626a1702b9f61f234c5d102d8cfbd
jfries <jonathan.fries@gmail.com>
2019-08-02 16:09:56 -0600

    Changes for Server-Side rendering of Chuckles chat.

--- routes/chuckleschat.js
+++ routes/chuckleschat.js
@@ -4,13 +4,67 @@
 var express = require('express');
 var router = express.Router();
 
+const Datastore = require('@google-cloud/datastore');
+
+// Instantiates a client
+const ds = Datastore(
+    {projectId: 'chucklesthedirtchipmuffin'}
+);
+
+ds.namespace = 'Chuckles';
+
+
 /* GET home page. */
 router.get('/:id*?', function(req, res, next) {
-    res.render('chuckleschat', {
-        title: 'Chuckles Chat',
-        NeedsJquery: 'true',
-        maxChat: global.maxChat,
+
+    var chatId = req.params.id;
+
+    if(!chatId)
+    {
+      chatId  = getRandomIntInclusive(1, global.maxChat);
+    }
+
+    randomNumber = getRandomIntInclusive(1, 21);
+
+    console.log("Chat ID = " + chatId);
+
+    var query = ds.createQuery(['ChucklesQuotes'])
+        .filter('ListNumber', '=', parseInt(chatId))
+        .order('ListNumber');
+
+        ds.runQuery(query, (err, entities, nextQuery) => {
+            if (err) {
+                console.log(err);
+                err.status = 404;
+                res.render('error');
+                return;
+            }
+
+            if(entities.length > 0 ) {
+
+              console.log("We got something back.  Sweet!");
+              console.log("Quote = " + entities[0].QuoteText);
+
+              res.render('chuckleschat', {
+                  title: 'Chuckles Chat',
+                  quoteText: entities[0].QuoteText,
+                  picture: 'https://storage.googleapis.com/chucklesthedirtchipmuffin.com/ChatPix/ChucklesChat' + randomNumber + '.png',
+                  NeedsJquery: 'true',
+                  maxChat: global.maxChat,
+              });
+            }
+            else
+            {
+                console.log("Nothing returned from data store.");
+                res.render('error');
+            }
     });
 });
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 module.exports = router;
